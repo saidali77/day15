13ef44c190dca4a31b33f1117d6d585d52808d24
saidali77 <mamedovsaidali7@gmail.com>
2020-07-17 16:46:42 +0600

    test15

---
+++ Task8.py
@@ -0,0 +1,10 @@
+given_number = int(input())
+if given_number % 3 == 0:
+    if given_number % 5 == 0:
+        print ("HahaHo")
+elif given_number % 3 == 0:
+    print("Haha")
+elif given_number % 5 == 0:
+    print("Hoo")
+else:
+    print("Aaaa")                     
